3738bfcb4c7cc46a68d276cfb915957023be62c9
NjeriMarion <njeriwmarion@gmail.com>
2023-04-03 07:11:55 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,24 @@
 fun main(){
-    modules()
+    name()
+    num(1,2)
+nums(1,2,3,4)
+fact()
 
 }
-fun modules(){
-    var num1=240
-    var num2=45
-    println(num1%num2)
-
+fun name() {
+    var name = "Hello Ada"
+    println(name)
+}
+fun num(num1:Int,num2:Int){
+    var num=num1%num2
+    println(num)
 }
 
-
-
+fun nums(nums1:Int,nums2:Int,nums3:Int,nums4:Int):Int{
+ var nums1 =nums1+nums2+nums3+nums4
+ return nums1
+}
+fun fact() {
+    var fact = "I love AkiraChix"
+    println(fact)
+}
